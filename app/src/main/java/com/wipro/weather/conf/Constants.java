2e193c524b24fdee6ee358c615468d33d8835415
Oliver-Henry <36078057+Oliver-Henry@users.noreply.github.com>
2018-06-21 11:25:07 +0100

    Initial commit

---
+++ app/src/main/java/com/wipro/weather/conf/Constants.java
@@ -0,0 +1,36 @@
+package com.wipro.weather.conf;
+
+/**
+ * Static constants variables.
+ */
+public class Constants {
+
+    /**
+     * API KEY
+     */
+    public final static String API_KEY = "3702516f96825e5ef697a40d238736c7";
+
+    /**
+     * Weather webserver
+     */
+    public final static String URL_WEATHER = "http://api.openweathermap.org/data/2.5/";
+
+    /**
+     * Weather webserver
+     */
+    public final static String URL_IMG_WEATHER = "http://openweathermap.org/img/w/";
+    /**
+     * Weather extension
+     */
+    public final static String EXT_WEATHER = ".png";
+
+    /**
+     * 5 day forecast is available at any location or city
+     * 5 day forecast includes weather data every 3 hours
+     * Forecast is available in JSON
+     */
+    public final static String URL_5_DAYS = "forecast?q=London&appid=";
+
+
+
+}
